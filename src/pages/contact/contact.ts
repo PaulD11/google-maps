fcdc19cfb05350a50cb09c0c0ad31aa95be6d090
Laura Meyer <laura.meyer@hs-furtwangen.de>
2018-11-20 21:02:13 +0100

    LM18 done gui and backend logout

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  private user: firebase.User;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    private afAuth: AngularFireAuth,
+    private navCtrl: NavController,
+    ) {
+    afAuth.authState.subscribe(user => {
+      this.user = user;
+      console.log(this.user);
+    });
   }
 
+  signOut() {
+    this.afAuth.auth.signOut()
+      .then(() => {
+        this.navCtrl.setRoot(LoginPage);
+      }).catch(() => {
+        console.log("error");;
+      })
+
+}
 }
