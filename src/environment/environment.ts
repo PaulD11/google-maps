27d90625b3572ed6289a07bc2d51851ceb279e5f
Laura Meyer <laura.meyer@hs-furtwangen.de>
2018-11-20 20:22:24 +0100

    added "forgot password" gui and method

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,10 +1,6 @@
 export const firebaseConfig = {
     fire: {
-        apiKey: "xxxxxxx",
-        authDomain: "xxxxxx",
-        databaseURL: "xxxxxx",
-        projectId: "xxxxxxx",
-        storageBucket: "xxxxxxxx",
-        messagingSenderId: "xxxxx"
+
+    
     }
 };
