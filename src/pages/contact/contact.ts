7fb1727f0f04ada11443441d549e28310fbf3081
Laura Meyer <laura.meyer@hs-furtwangen.de>
2018-11-21 19:39:53 +0100

    LM18-Logout button at tabs

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,32 +1,13 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { LoginPage } from '../login/login';
-import { AngularFireAuth } from 'angularfire2/auth';
+
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-  private user: firebase.User;
 
-  constructor(
-    private afAuth: AngularFireAuth,
-    private navCtrl: NavController,
-    ) {
-    afAuth.authState.subscribe(user => {
-      this.user = user;
-      console.log(this.user);
-    });
+
+  constructor() {
   }
-
-  signOut() {
-    this.afAuth.auth.signOut()
-      .then(() => {
-        this.navCtrl.setRoot(LoginPage);
-      }).catch(() => {
-        console.log("error");;
-      })
-
-}
 }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,17 +3,35 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-
+  private user: firebase.User;
   tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
 
-  constructor() {
+  constructor(private afAuth: AngularFireAuth,
+    private navCtrl: NavController,
+  ) {
+    afAuth.authState.subscribe(user => {
+      this.user = user;
+      console.log(this.user);
+    });
+
+  }
+  signOut() {
+    this.afAuth.auth.signOut()
+      .then(() => {
+        this.navCtrl.setRoot(LoginPage);
+      }).catch(() => {
+        console.log("error");;
+      })
 
   }
 }
