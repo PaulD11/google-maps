2d07db15e3953d33e6bc2eea0743356f97c23da0
michael-schilling@hotmail.de <michael-schilling@hotmail.de>
2018-11-23 22:20:04 +0100

    LM30-Map Add Google Maps

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,10 +1,5 @@
 export const firebaseConfig = {
     fire: {
-        apiKey: "xxxxxxx",
-        authDomain: "xxxxxx",
-        databaseURL: "xxxxxx",
-        projectId: "xxxxxxx",
-        storageBucket: "xxxxxxxx",
-        messagingSenderId: "xxxxx"
+
     }
 };
