7d6aa5f1daf16fd61908b09fcd8200dd21b091b0
Laura Meyer <laura.meyer@hs-furtwangen.de>
2018-11-27 13:04:42 +0100

    Travis Test Laura

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,10 +1,5 @@
 export const firebaseConfig = {
     fire: {
-        apiKey: "AIzaSyC70-By14poBJrguMMQMpHcyNhiA-ir9qg",
-        authDomain: "bam20182019-f5c79.firebaseapp.com",
-        databaseURL: "https://bam20182019-f5c79.firebaseio.com",
-        projectId: "bam20182019-f5c79",
-        storageBucket: "bam20182019-f5c79.appspot.com",
-        messagingSenderId: "234706923908"
+
     }
 };
