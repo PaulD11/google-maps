d44bf8570710d5fc68c191d1a24a99148e8f5da8
Laura Meyer <laura.meyer@hs-furtwangen.de>
2019-01-17 14:08:10 +0100

    selenium

--- e2e/LM-12 Login/LM-12_Login.js
+++ e2e/LM-12 Login/LM-12_Login.js
@@ -10,20 +10,16 @@ describe('Login', function () {
 
         //LOGOUT
         element(by.id('logout')).click();
-        
-        browser.driver.sleep(5000);
+    
 
         element(By.id("email")).sendKeys("laura.meyer@hs-furtwangen.de");
         element(By.id("password")).sendKeys("123456789");
 
 
-        browser.driver.sleep(5000);
         element(by.id('login')).click();
 
-        browser.driver.sleep(5000);
 
         //expected Conditions
-
         let EC = protractor.ExpectedConditions;
         var logoutButton = element(by.id('logout'));
         browser.wait(EC.elementToBeClickable(logoutButton), 10000);
--- e2e/conf.js
+++ e2e/conf.js
@@ -2,7 +2,7 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: [
     'LM-18 Logout/LM-18_Logout.js',
-      'LM-12 Login/LM-12_Login.js'
+     'LM-12 Login/LM-12_Login.js'
   ]
 };
 
